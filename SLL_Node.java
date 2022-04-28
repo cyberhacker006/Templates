51f68d720c1cfd9136758fedb0a8e3c86ad41461
Berkay Güzel <98205992+cyberhacker006@users.noreply.github.com>
2022-04-28 13:17:05 +0300

    Add files via upload
    This SingleLinkListNode works with SingleLinkList class.

---
+++ SLL_Node.java
@@ -0,0 +1,20 @@
+public class SLL_Node{
+		private Object data;
+		private SLL_Node link;
+		public SLL_Node(Object dataToAdd){
+			this.data = dataToAdd;
+			this.link = null;
+		}
+		public Object getData() {
+			return data;
+		}
+		public void setData(Object data) {
+			this.data = data;
+		}
+		public SLL_Node getLink() {
+			return link;
+		}
+		public void setLink(SLL_Node link) {
+			this.link = link;
+		}
+	}
